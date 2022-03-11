c5c167d794ffb33034a00434c6854789c96ac07b
Daniel Gonzalez Gregores <dgonzalezgregores@danielcastelao.org>
2022-03-11 09:12:35 +0100

    pedir nombre del repo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,6 +9,7 @@ public class Main {
     public static void main(String[] args) {
         GitHub github = null;
         try {
+            //Pedimos el token al usuario
             String token = JOptionPane.showInputDialog("Introduce el token de GitHub: ");
         //    String pathAlFich = "/home/dam1/IdeaProjects/CrearProyecto/ficheroToken.propierties";
             github = new GitHubBuilder()
@@ -21,9 +22,11 @@ public class Main {
 
         GHRepository repo = null;
         try {
+            //El usuario debe introducir el nombre del repositorio que quiera crear
+            String nomRe = JOptionPane.showInputDialog("Introduce el nombre del repositorio: ");
             //GHRepository repo2 = github.createRepository("miRepo").create();
             repo = github.createRepository(
-                    "RepositorioNuevo", "Este es el nuevo repositorio",
+                    nomRe, "Este es el nuevo repositorio",
                     "https://www.kohsuke.org/", true/*public*/);
         } catch (IOException e) {
             e.printStackTrace();
