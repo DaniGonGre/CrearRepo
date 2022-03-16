06e270d923bcaccb803d6a362213bb84680dd15f
Daniel Gonzalez Gregores <dgonzalezgregores@danielcastelao.org>
2022-03-16 13:00:52 +0100

    arreglado fichero

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,36 +1,20 @@
-import org.kohsuke.github.GHRepository;
-import org.kohsuke.github.GitHub;
-import org.kohsuke.github.GitHubBuilder;
-
 import javax.swing.*;
-import java.io.IOException;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        GitHub github = null;
-        try {
-            //Pedimos el token al usuario
-            String token = JOptionPane.showInputDialog("Introduce el token de GitHub: ");
-        //    String pathAlFich = "/home/dam1/IdeaProjects/CrearProyecto/ficheroToken.propierties";
-            github = new GitHubBuilder()
-                    .withOAuthToken(token)
-             //       .fromPropertyFile(pathAlFich)
-                    .build();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
 
-        GHRepository repo = null;
-        try {
-            //El usuario debe introducir el nombre del repositorio que quiera crear
-            String nomRe = JOptionPane.showInputDialog("Introduce el nombre del repositorio: ");
-            //GHRepository repo2 = github.createRepository("miRepo").create();
-            repo = github.createRepository(
-                    nomRe, "Este es el nuevo repositorio",
-                    "https://www.kohsuke.org/", true/*public*/);
-        } catch (IOException e) {
-            e.printStackTrace();
+
+    public static void main(String[] args) {
+        File file = new File("token.properties");
+        String pathFile = "token.properties";
+        Escribir escribir = new Escribir();
+        if (file.exists()) {
+            escribir.crear(pathFile);
+        } else {
+            escribir.escribir(pathFile);
+            escribir.crear(pathFile);
         }
     }
 
 }
+
